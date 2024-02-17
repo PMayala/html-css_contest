9cc4a8ab30a896bdfdb2a3040e4b13fb47edf561
gyhaan <g.yhaan@alustudent.com>
2024-02-17 21:01:43 +0200

    fixing form

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector(".open").addEventListener("click", function () {
-    document.querySelector("nav").classList.toggle("open");
-  });
-});
-
-console.log(document.querySelector(".open"));
